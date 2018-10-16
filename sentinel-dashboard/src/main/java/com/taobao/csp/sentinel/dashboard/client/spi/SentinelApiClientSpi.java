2c904a36cb90dcba8940ad0fbf8c1c0230164f81
wangle_r@163.com <wangle_r@163.com>
2018-10-16 11:10:17 +0800

    add SentinelApiClientSpi

---
+++ sentinel-dashboard/src/main/java/com/taobao/csp/sentinel/dashboard/client/spi/SentinelApiClientSpi.java
@@ -0,0 +1,92 @@
+package com.taobao.csp.sentinel.dashboard.client.spi;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import com.alibaba.csp.sentinel.command.vo.NodeVo;
+import com.taobao.csp.sentinel.dashboard.datasource.entity.rule.AuthorityRuleEntity;
+import com.taobao.csp.sentinel.dashboard.datasource.entity.rule.DegradeRuleEntity;
+import com.taobao.csp.sentinel.dashboard.datasource.entity.rule.FlowRuleEntity;
+import com.taobao.csp.sentinel.dashboard.datasource.entity.rule.ParamFlowRuleEntity;
+import com.taobao.csp.sentinel.dashboard.datasource.entity.rule.SystemRuleEntity;
+
+public interface SentinelApiClientSpi {
+    
+    public List<NodeVo> fetchResourceOfMachine(String ip, int port, String type);
+
+    /**
+     * Fetch cluster node.
+     *
+     * @param ip          ip to fetch
+     * @param port        port of the ip
+     * @param includeZero whether zero value should in the result list.
+     * @return
+     */
+    public List<NodeVo> fetchClusterNodeOfMachine(String ip, int port, boolean includeZero);
+
+    public List<FlowRuleEntity> fetchFlowRuleOfMachine(String app, String ip, int port) ;
+
+    public List<DegradeRuleEntity> fetchDegradeRuleOfMachine(String app, String ip, int port);
+
+    public List<SystemRuleEntity> fetchSystemRuleOfMachine(String app, String ip, int port) ;
+
+    /**
+     * Fetch all parameter flow rules from provided machine.
+     *
+     * @param app  application name
+     * @param ip   machine client IP
+     * @param port machine client port
+     * @return all retrieved parameter flow rules
+     * @since 0.2.1
+     */
+    public CompletableFuture<List<ParamFlowRuleEntity>> fetchParamFlowRulesOfMachine(String app, String ip, int port) ;
+
+    /**
+     * Fetch all authority rules from provided machine.
+     *
+     * @param app  application name
+     * @param ip   machine client IP
+     * @param port machine client port
+     * @return all retrieved authority rules
+     * @since 0.2.1
+     */
+    public List<AuthorityRuleEntity> fetchAuthorityRulesOfMachine(String app, String ip, int port) ;
+
+    /**
+     * set rules of the machine. rules == null will return immediately;
+     * rules.isEmpty() means setting the rules to empty.
+     *
+     * @param app
+     * @param ip
+     * @param port
+     * @param rules
+     * @return whether successfully set the rules.
+     */
+    public boolean setFlowRuleOfMachine(String app, String ip, int port, List<FlowRuleEntity> rules) ;
+
+    /**
+     * set rules of the machine. rules == null will return immediately;
+     * rules.isEmpty() means setting the rules to empty.
+     *
+     * @param app
+     * @param ip
+     * @param port
+     * @param rules
+     * @return whether successfully set the rules.
+     */
+    public boolean setDegradeRuleOfMachine(String app, String ip, int port, List<DegradeRuleEntity> rules) ;
+
+    /**
+     * set rules of the machine. rules == null will return immediately;
+     * rules.isEmpty() means setting the rules to empty.
+     *
+     * @param app
+     * @param ip
+     * @param port
+     * @param rules
+     * @return whether successfully set the rules.
+     */
+    public boolean setSystemRuleOfMachine(String app, String ip, int port, List<SystemRuleEntity> rules) ;
+
+    public CompletableFuture<Void> setParamFlowRuleOfMachine(String app, String ip, int port, List<ParamFlowRuleEntity> rules);
+}
